327c6c33e488fb97f16bd2b10b587ce7638bda2d
= <lev605@icloud.com>
2024-01-24 16:58:20 +0300

    проведены работы по настраиванию socket сервера и клиента

--- online_socket/client_try.py
+++ online_socket/client_try.py
@@ -1,23 +1,42 @@
-import socket
-import json
+import socket  # Импортируем модуль для работы с сетевыми соединениями
+import json  # Импортируем модуль для работы с данными в формате JSON
+import time
 
 
 def start_client():
+    # Создаем сокет. Это как телефон, который можно использовать для звонков.
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Подключаемся к серверу. 'localhost' и 12345 - это адрес и порт сервера.
     client_socket.connect(('localhost', 12345))
 
-    # Начальное состояние игры
-    game_state = {'field': [0, 0, 0, 0, 0, 0, 0, 0, 0]}
+    # Начальное состояние игры - поле для игры и состояние игры.
+    game_state = {'field': [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                  'condition': 'ожидаю подтверждения'}
+    count = 1
+    while True:
+        # Преобразуем начальное состояние игры в формат JSON и отправляем серверу.
+        # Это как отправка сообщения с описанием игры.
+        data = json.dumps(game_state).encode('utf-8')
+        client_socket.send(data)
 
-    # Отправка начального состояния серверу
-    data = json.dumps(game_state).encode('utf-8')
-    client_socket.send(data)
+        # Ждем ответа от сервера и получаем обновленное состояние игры.
+        # Это как получение ответного сообщения.
+        updated_state = client_socket.recv(1024)
+        game_state = json.loads(updated_state.decode('utf-8'))
 
-    # Получение обновленного состояния от сервера
-    updated_state = client_socket.recv(1024)
-    game_state = json.loads(updated_state.decode('utf-8'))
-    print("Обновленное состояние от сервера: ", game_state)
+        # Печатаем обновленное состояние игры, полученное от сервера.
+        print("Обновленное состояние от сервера: ", game_state)
+        time.sleep(5)
+        try:
+            # Обновляем состояние игры. Например, ставим 'X' в ячейку поля.
+            game_state['field'][count] = 'X'
+            count += 2
+        except IndexError:
+            print('Закончились свободные поля, игра окончена')
+            break
 
+    # Закрываем соединение с сервером. Это как завершение звонка.
     client_socket.close()
 
 
